1658ab651ac8fba1af7165eeefed4614928f62fd
Mallen Arenas <mallenarenasc@gmail.com>
2022-12-26 12:13:07 -0300

    add update songs.json

--- index.js
+++ index.js
@@ -2,8 +2,22 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+
 app.listen(3000, console.log("¡Servidor encendido!"));
+
+app.use(express.json())
+
+// Leyendo songs.json
 app.get("/canciones", (req, res) => {
     const songs = JSON.parse(fs.readFileSync("songs.json"));
     res.json(songs);
+  });
+
+// Actualizando songs.json
+app.post("/canciones", (req, res) => {
+    const song = req.body;
+    const songs = JSON.parse(fs.readFileSync("songs.json"));
+    songs.push(song);
+    fs.writeFileSync("songs.json", JSON.stringify(songs));
+    res.send("Canción agregada con éxito!");
   });
