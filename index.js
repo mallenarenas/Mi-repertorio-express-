74a1beedb33f136dc93194ff53df7c4c73ee0522
Mallen Arenas <mallenarenasc@gmail.com>
2022-12-26 12:41:13 -0300

    add update to songs.json

--- index.js
+++ index.js
@@ -13,11 +13,30 @@ app.get("/canciones", (req, res) => {
     res.json(songs);
   });
 
-// Actualizando songs.json
+// Creando songs.json
 app.post("/canciones", (req, res) => {
     const song = req.body;
     const songs = JSON.parse(fs.readFileSync("songs.json"));
     songs.push(song);
     fs.writeFileSync("songs.json", JSON.stringify(songs));
     res.send("Canción agregada con éxito!");
+  });
+// Borrando canciones en songs.jsong
+app.delete("/canciones/:id", (req, res) => {
+    const { id } = req.params;
+    const songs = JSON.parse(fs.readFileSync("songs.json"));
+    const index = songs.findIndex((s) => s.id == id);
+    songs.splice(index, 1);
+    fs.writeFileSync("songs.json", JSON.stringify(songs));
+    res.send("Canción eliminada con éxito");
+  });
+// Actualizando una Canción en songs.json
+app.put("/canciones/:id", (req, res) => {
+    const { id } = req.params;
+    const song = req.body;
+    const songs = JSON.parse(fs.readFileSync("songs.json"));
+    const index = songs.findIndex((s) => s.id == id);
+    songs[index] = song;
+    fs.writeFileSync("songs.json", JSON.stringify(songs));
+    res.send("Canción modificada con éxito");
   });
