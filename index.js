84bebab419baee4a790bccb4fa1f76d6f8ff4415
Mallen Arenas <mallenarenasc@gmail.com>
2022-12-26 11:48:33 -0300

    read songs.json

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 app.listen(3000, console.log("¡Servidor encendido!"));
+app.get("/canciones", (req, res) => {
+    const songs = JSON.parse(fs.readFileSync("songs.json"));
+    res.json(songs);
+  });
